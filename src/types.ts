b9d6423a30778c962f2d35f6b9fa3bf5cca03f48
Joshua Pfefferkorn <77121865+jpfeff@users.noreply.github.com>
2023-09-15 02:32:26 -0400

    fetch course info produces entire course object

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,15 @@
-export interface Class {
+export interface IClass {
   subjectCode: string;
   courseNum: string;
   classTitle: string;
   instructor: string;
   periodCode: string;
   location: string;
+  main?: Period;
+  xHour?: Period;
+}
+
+export interface Period {
+  time: [number, number, number, number];
+  days: string[];
 }
