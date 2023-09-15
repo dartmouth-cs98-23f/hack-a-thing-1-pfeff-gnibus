7da7e092efc1792a3d656a3191ba9de38e5aaa47
jgnibo <john.m.gnibus.24@dartmouth.edu>
2023-09-15 16:36:18 -0400

    updating course schedule

--- src/constants/courseSchedule.ts
+++ src/constants/courseSchedule.ts
@@ -3,146 +3,146 @@
 import { Period } from '../types';
 
 const courseSchedule: { [key: string]: { class: Period; xHour: Period } } = {
-    '8S': {
-      class: {
-        time: [7, 45, 0, 50],
-        days: ['M', 'TU', 'TH', 'F'],
-      },
-      xHour: {
-        time: [7, 45, 0, 50],
-        days: ['W'],
-      },
+  '8S': {
+    class: {
+      time: [7, 45, 0, 50],
+      days: ['MO', 'TU', 'TH', 'FR'],
     },
-    '8L': {
-      class: {
-        time: [7, 30, 1, 50],
-        days: ['M', 'W', 'F'],
-      },
-      xHour: {
-        time: [7, 45, 0, 50],
-        days: ['TH'],
-      },
+    xHour: {
+      time: [7, 45, 0, 50],
+      days: ['WE'],
     },
-    '9S': {
-      class: {
-        time: [9, 5, 0, 50],
-        days: ['M', 'TU', 'TH', 'F'],
-      },
-      xHour: {
-        time: [9, 5, 0, 50],
-        days: ['W'],
-      },
+  },
+  '8L': {
+    class: {
+      time: [7, 30, 1, 50],
+      days: ['MO', 'WE', 'FR'],
     },
-    '9L': {
-      class: {
-        time: [8, 50, 1, 5],
-        days: ['M', 'W', 'F'],
-      },
-      xHour: {
-        time: [9, 5, 0, 50],
-        days: ['TH'],
-      },
+    xHour: {
+      time: [7, 45, 0, 50],
+      days: ['TH'],
     },
-    "10": {
-      class: {
-        time: [10, 10, 1, 5],
-        days: ['M', 'W', 'F'],
-      },
-      xHour: {
-        time: [12, 15, 0, 50],
-        days: ['TH'],
-      },
+  },
+  '9S': {
+    class: {
+      time: [9, 5, 0, 50],
+      days: ['MO', 'TU', 'TH', 'FR'],
     },
-    "11": {
-      class: {
-        time: [11, 30, 1, 5],
-        days: ['M', 'W', 'F'],
-      },
-      xHour: {
-        time: [12, 15, 0, 50],
-        days: ['TU'],
-      },
+    xHour: {
+      time: [9, 5, 0, 50],
+      days: ['WE'],
     },
-    "12": {
-      class: {
-        time: [12, 50, 1, 5],
-        days: ['M', 'W', 'F'],
-      },
-      xHour: {
-        time: [13, 20, 0, 50],
-        days: ['TU'],
-      },
+  },
+  '9L': {
+    class: {
+      time: [8, 50, 1, 5],
+      days: ['MO', 'WE', 'FR'],
     },
-    "2": {
-      class: {
-        time: [14, 10, 1, 5],
-        days: ['M', 'W', 'F'],
-      },
-      xHour: {
-        time: [13, 20, 0, 50],
-        days: ['TH'],
-      },
+    xHour: {
+      time: [9, 5, 0, 50],
+      days: ['TH'],
     },
-    "10A": {
-      class: {
-        time: [10, 10, 1, 50],
-        days: ['TU', 'TH'],
-      },
-      xHour: {
-        time: [15, 30, 0, 50],
-        days: ['F'],
-      },
+  },
+  "10": {
+    class: {
+      time: [10, 10, 1, 5],
+      days: ['MO', 'WE', 'FR'],
     },
-    "2A": {
-      class: {
-        time: [14, 25, 1, 50],
-        days: ['TU', 'TH'],
-      },
-      xHour: {
-        time: [17, 30, 0, 50],
-        days: ['W'],
-      },
+    xHour: {
+      time: [12, 15, 0, 50],
+      days: ['TH'],
     },
-    "3A": {
-      class: {
-        time: [15, 30, 1, 50],
-        days: ['M', 'W'],
-      },
-      xHour: {
-        time: [17, 30, 0, 50],
-        days: ['M'],
-      },
+  },
+  "11": {
+    class: {
+      time: [11, 30, 1, 5],
+      days: ['MO', 'WE', 'FR'],
     },
-    "3B": {
-      class: {
-        time: [16, 30, 1, 50],
-        days: ['TU', 'TH'],
-      },
-      xHour: {
-        time: [16, 35, 0, 50],
-        days: ['F'],
-      },
+    xHour: {
+      time: [12, 15, 0, 50],
+      days: ['TU'],
     },
-    "6A": {
-      class: {
-        time: [18, 30, 1, 50],
-        days: ['M', 'TH'],
-      },
-      xHour: {
-        time: [18, 30, 0, 50],
-        days: ['TU'],
-      },
+  },
+  "12": {
+    class: {
+      time: [12, 50, 1, 5],
+      days: ['MO', 'WE', 'FR'],
     },
-    "6B": {
-      class: {
-        time: [18, 30, 3, 0],
-        days: ['W'],
-      },
-      xHour: {
-        time: [19, 30, 0, 50],
-        days: ['TU'],
-      },
-    },   
-  }
+    xHour: {
+      time: [13, 20, 0, 50],
+      days: ['TU'],
+    },
+  },
+  "2": {
+    class: {
+      time: [14, 10, 1, 5],
+      days: ['MO', 'WE', 'FR'],
+    },
+    xHour: {
+      time: [13, 20, 0, 50],
+      days: ['TH'],
+    },
+  },
+  "10A": {
+    class: {
+      time: [10, 10, 1, 50],
+      days: ['TU', 'TH'],
+    },
+    xHour: {
+      time: [15, 30, 0, 50],
+      days: ['FR'],
+    },
+  },
+  "2A": {
+    class: {
+      time: [14, 25, 1, 50],
+      days: ['TU', 'TH'],
+    },
+    xHour: {
+      time: [17, 30, 0, 50],
+      days: ['WE'],
+    },
+  },
+  "3A": {
+    class: {
+      time: [15, 30, 1, 50],
+      days: ['MO', 'WE'],
+    },
+    xHour: {
+      time: [17, 30, 0, 50],
+      days: ['MO'],
+    },
+  },
+  "3B": {
+    class: {
+      time: [16, 30, 1, 50],
+      days: ['TU', 'TH'],
+    },
+    xHour: {
+      time: [16, 35, 0, 50],
+      days: ['FR'],
+    },
+  },
+  "6A": {
+    class: {
+      time: [18, 30, 1, 50],
+      days: ['MO', 'TH'],
+    },
+    xHour: {
+      time: [18, 30, 0, 50],
+      days: ['TU'],
+    },
+  },
+  "6B": {
+    class: {
+      time: [18, 30, 3, 0],
+      days: ['WE'],
+    },
+    xHour: {
+      time: [19, 30, 0, 50],
+      days: ['TU'],
+    },
+  },
+}
 
 export default courseSchedule;
