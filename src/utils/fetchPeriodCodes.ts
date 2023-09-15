7667753433dcfb2456f50a1e622c36e2101b79c5
Joshua Pfefferkorn <77121865+jpfeff@users.noreply.github.com>
2023-09-14 23:55:56 -0400

    trying to get course name in addition to period code

--- src/utils/fetchPeriodCodes.ts
+++ src/utils/fetchPeriodCodes.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import courseSchedule from '../constants/courseSchedule';
 
-export default async function fetchPeriodCodes(subj: string, crsenum: string): Promise<string[]> {
+export default async function fetchPeriodCodes(subj: string, crsenum: string): Promise<{ courseName: string, periodCode: string }[]> {
 
   const url = 'http://localhost:3000/fetchPeriodCodesProxy'; // Updated URL to use your server-side proxy route
   const data = new URLSearchParams();
@@ -19,12 +19,18 @@ export default async function fetchPeriodCodes(subj: string, crsenum: string): P
 
     const periodCodeElements = doc.querySelectorAll('a[href^="https://www.dartmouth.edu/reg/docs/class_schedule_22f.pdf"]');
 
-    const periodCodes: string[] = [];
+    const periodCodes: { courseName: string, periodCode: string }[] = [];
 
     periodCodeElements.forEach(element => {
       const periodCode = element.textContent?.trim() || '';
-      if (Object.keys(courseSchedule).includes(periodCode) && !periodCodes.includes(periodCode)) {
-        periodCodes.push(periodCode);
+      if (Object.keys(courseSchedule).includes(periodCode)) {
+        // Find the closest parent div with class 'data-table'
+        const dataTableDiv = element.closest('div.data-table');
+        if (dataTableDiv) {
+          const courseNameElement = dataTableDiv.querySelector('td:nth-child(6) a');
+          const courseName = courseNameElement?.textContent?.trim() || '';
+          periodCodes.push({ courseName, periodCode });
+        }
       }
     });
 
