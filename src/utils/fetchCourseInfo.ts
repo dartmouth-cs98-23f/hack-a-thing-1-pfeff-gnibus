4260ce975a80160351912fbcefb1bf8f3d916591
Joshua Pfefferkorn <77121865+jpfeff@users.noreply.github.com>
2023-09-15 00:47:35 -0400

    cleanup

--- src/utils/fetchCourseInfo.ts
+++ src/utils/fetchCourseInfo.ts
@@ -47,7 +47,6 @@ export default async function fetchCourseInfo(subj: string, crsenum: string): Pr
           roomNumber: roomDiv?.textContent?.trim() || '',
         };
       
-      console.log(course);
       results.push(course)
       }
     }
