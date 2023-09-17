6d39232fcfdd7f6bd4c56a4af990500a62ca2860
Joshua Pfefferkorn <77121865+jpfeff@users.noreply.github.com>
2023-09-16 22:19:39 -0400

    changed syntax on main

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-  BrowserRouter, Routes, Route,
-} from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import Instructions from './Instructions';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
       <header className='header'>
         <h1>Calendarize</h1>
         <p className='header-subtext'>Course calendar generator for Dartmouth students</p>
       </header>
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/instructions" element={<Instructions />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
