7c5dfe0f60081c30253bf976bb8a9436b8456e86
Joshua Pfefferkorn <77121865+jpfeff@users.noreply.github.com>
2023-09-16 22:08:18 -0400

    fixing routes

--- src/main.tsx
+++ src/main.tsx
@@ -9,11 +9,11 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
       <header className='header'>
         <h1>Calendarize</h1>
         <p className='header-subtext'>Course calendar generator for Dartmouth students</p>
       </header>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/instructions" element={<Instructions />} />
